ceae7d6e4a9ee85b263b9301d2fc475ac98708a7
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2026-03-16 19:03:09 +0530

    2nd way of first pattern

--- src/Patterns/first.java
+++ src/Patterns/first.java
@@ -12,7 +12,12 @@ public class first {
         int j;
 
         for(i=0;i<4;i++){
-            System.out.println("****");  //either we can do this
+           //or if we use two loops
+            for(j=0;j<4;j++){
+                System.out.print("*"); //this loop prints * 4 times
+            }
+            System.out.println(); //inner loop goes to outer loop to check condition after 4 * r printed this line moves
+            //row 0 -> row 1
         }
     }
 }
