26080723df9a9cfbd5cfdd708fc00633ceeec273
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2026-03-16 22:04:13 +0530

    4th pattern(tricky one)

--- src/Patterns/second.java
+++ src/Patterns/second.java
@@ -3,17 +3,27 @@ package Patterns;
 public class second {
     public static void main(String[] args) {
         /*
-        "*****
-         ****
-         ***
-         **
-         *
+        "    *
+            ***
+           *****
+          *******
+         *********
          */
         int i;
         int j;
-        for(i=0;i<5;i++){
-            for(j=i;j<5;j++){
-                System.out.print(i+" ");
+        int n = 5;
+        for(i=0;i<n;i++){
+            //space
+            for(j=0;j<n-i-1;j++) {
+                System.out.print(" ");
+            }
+            //stars
+            for(j=0;j<2*i+1;j++) {
+                System.out.print("*");
+            }
+            //space
+            for(j=0;j<n-i-1;j++) {
+                System.out.print(" ");
             }
             System.out.println();
         }
