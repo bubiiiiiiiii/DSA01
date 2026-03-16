f2be86fc2753da1ed91168e7a14fde9bf5c43dff
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2026-03-16 22:48:09 +0530

    7th pattern *did this shit on my own bitches😎😎.*

--- src/Patterns/second.java
+++ src/Patterns/second.java
@@ -2,50 +2,29 @@ package Patterns;
 
 public class second {
     public static void main(String[] args) {
-        /*    *
-             ***
-            *****
-           *******
-          *********
-          *********
-           *******
-            *****
-             ***
-              *
+        /* *
+           **
+           ***
+           *****
+           ***
+           **
+           *
          */
         int i;
         int j;
-        int n = 4;
-        for(i=0;i<n;i++){
-            //space
-            for(j=0;j<n-i-1;j++) {
-                System.out.print(" ");
-            }
-            //stars
-            for(j=0;j<2*i+1;j++){
+        int n =7;
+        for(i=0;i<n;i++) {
+            for(j=0;j<i;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
-
-            //these two space loops were printing the pattern besides each other instead of in continuation
-           /* //space
-            for(j=0;j<n-i-1;j++) {
-                System.out.print(" ");
+        for(i=0;i<n;i++){
+            for(j=i;j<n;j++){
+                System.out.print("*");
             }
-            for(j=0;j<i;j++){
-                System.out.print(" ");
-            }  */
-            //we'll make another outer loop for the inverted pyramid
-            for(i=0;i<n;i++){
-                for(j=0;j<i;j++){
-                    System.out.print(" ");
-                }
-                for(j=0;j<2*n-2*i-1;j++){
-                    System.out.print("*");
-                }
-                System.out.println();
-            }
+            System.out.println();
+        }
         }
     }
 
