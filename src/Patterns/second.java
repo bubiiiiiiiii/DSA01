b1037092fe9699df943f5374b4bf82de4a5e3434
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2026-03-16 22:39:37 +0530

    6th pattern(bherrryyyy difficult)

--- src/Patterns/second.java
+++ src/Patterns/second.java
@@ -2,30 +2,50 @@ package Patterns;
 
 public class second {
     public static void main(String[] args) {
-        /*
-        "*********
-          *******
-           *****
-            ***
-             *
+        /*    *
+             ***
+            *****
+           *******
+          *********
+          *********
+           *******
+            *****
+             ***
+              *
          */
         int i;
         int j;
-        int n = 5;
+        int n = 4;
         for(i=0;i<n;i++){
             //space
-            for(j=0;j<i;j++) {
+            for(j=0;j<n-i-1;j++) {
                 System.out.print(" ");
             }
             //stars
-            for(j=0;j<2*n-2*i-1;j++){
+            for(j=0;j<2*i+1;j++){
                 System.out.print("*");
             }
-            //space
-          /*  for(j=0;j<n;j++) {  //can run the code successfully even without this line
-                System.out.print(" ");
-            }  */
             System.out.println();
         }
+
+            //these two space loops were printing the pattern besides each other instead of in continuation
+           /* //space
+            for(j=0;j<n-i-1;j++) {
+                System.out.print(" ");
+            }
+            for(j=0;j<i;j++){
+                System.out.print(" ");
+            }  */
+            //we'll make another outer loop for the inverted pyramid
+            for(i=0;i<n;i++){
+                for(j=0;j<i;j++){
+                    System.out.print(" ");
+                }
+                for(j=0;j<2*n-2*i-1;j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
     }
-}
+
