02151a130a2a940736fcd7050b3529bb08fe9828
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2026-03-16 18:59:53 +0530

    first pattern

---
+++ src/Patterns/first.java
@@ -0,0 +1,18 @@
+package Patterns;
+
+public class first {
+    /*
+      ****
+      ****
+      ****
+      ****
+     */
+    public static void main(String[] args) {
+        int i;
+        int j;
+
+        for(i=0;i<4;i++){
+            System.out.println("****");  //either we can do this
+        }
+    }
+}
