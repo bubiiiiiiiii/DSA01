ddb0beacbfd5d939c0577e4d9e75a085c1e02bbc
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2026-03-16 21:46:07 +0530

    3rd pattern

--- src/Patterns/second.java
+++ src/Patterns/second.java
@@ -3,17 +3,17 @@ package Patterns;
 public class second {
     public static void main(String[] args) {
         /*
-        "*
-         **
-         ***
+        "*****
          ****
-         *****
+         ***
+         **
+         *
          */
         int i;
         int j;
         for(i=0;i<5;i++){
-            for(j=0;j<i;j++){
-                System.out.print(j+" ");
+            for(j=i;j<5;j++){
+                System.out.print(i+" ");
             }
             System.out.println();
         }
