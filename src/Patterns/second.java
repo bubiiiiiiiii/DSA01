738a9aa3d781b15ba10ab8905e09152ed2d748dd
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2026-03-19 22:38:21 +0530

    10th pattern

--- src/Patterns/second.java
+++ src/Patterns/second.java
@@ -2,24 +2,20 @@ package Patterns;
 
 public class second {
     public static void main(String[] args) {
-        /*1
-          2 3
-          4 5 6
-          7 8 9 10
-          11 12 13 14 15
+        /*a
+          ab
+          abc
+          abcd
          */
         int i;
-        int j;
+        char j;
         int n=5;
-        int num=0;
-       for(i=1;i<n;i++){//so when i do i=0 there's a gap in the output thats cuz when i=0 then goes into inner loop
-           //j<i that means j<0 and j=0 not possible condition becomes false so no output gets printed but
-           for(j=0;j<i;j++){
-               num = num + 1;
-               System.out.print(num + " ");
+
+       for(i=1;i<n;i++){
+           for(j='A';j<'A'+ i;j++){
+               System.out.print(j);
            }
-           System.out.println(); //this line still prints leaving an empty line there
-           //that's why instead of i=0 we'll do i=1
+           System.out.println();
        }
 
         }
