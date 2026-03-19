e84bd7b739c0ac3d579c014e9512b5cde224df6c
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2026-03-19 22:27:43 +0530

    9th pattern

--- src/Patterns/second.java
+++ src/Patterns/second.java
@@ -2,27 +2,26 @@ package Patterns;
 
 public class second {
     public static void main(String[] args) {
-        /* 1
-           0 1
-           1 0 1
-           0 1 0 1
-           1 0 1 0 1
+        /*1
+          2 3
+          4 5 6
+          7 8 9 10
+          11 12 13 14 15
          */
         int i;
         int j;
-        int n =5;
-        int start = 1;
-        for(i=0;i<n;i++) {
-            if(i%2==0){
-               start = 1;
-            }else{
-                start = 0;
-            }
-            for(j=0;j<=i;j++){
-                System.out.print(start);
-                start = 1 - start;  // this line is very important it flips the digit if 0 then 1 if 1 then 0
-            }
-            System.out.println();}
+        int n=5;
+        int num=0;
+       for(i=1;i<n;i++){//so when i do i=0 there's a gap in the output thats cuz when i=0 then goes into inner loop
+           //j<i that means j<0 and j=0 not possible condition becomes false so no output gets printed but
+           for(j=0;j<i;j++){
+               num = num + 1;
+               System.out.print(num + " ");
+           }
+           System.out.println(); //this line still prints leaving an empty line there
+           //that's why instead of i=0 we'll do i=1
+       }
+
         }
 
     }
