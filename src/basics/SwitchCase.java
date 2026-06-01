374d3c8cbbedf224dec3fedbb49c514406170174
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2026-06-01 22:40:03 +0530

    swtich case

---
+++ src/basics/SwitchCase.java
@@ -0,0 +1,24 @@
+package basics;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        //basic knowledge
+        /*  switch(expression){
+            case1 :
+            //do something
+            break;
+            default:
+                //do something */
+
+        /*note:
+        1. cases have to be the same type as expression must be constant or literal
+        2. if break is not used it will continue to the next case
+         */
+    }
+}
+
+
+
+
+
+
