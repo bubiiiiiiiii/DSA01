edbd5ecabe88b4eed5019528899d88ef4b7c3416
Bubii <dubeybhoomi43@gmail.com>
2026-09-07 00:34:36 +0530

    Solved: Relative Sort Array [Easy]

---
+++ LeetCode/Easy/Relative Sort Array/solution.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] freq = new int[1001];
+
+        for (int num : arr1) {
+            freq[num]++;
+        }
+
+        int[] result = new int[arr1.length];
+        int index = 0;
+
+        for (int num : arr2) {
+            while (freq[num] > 0) {
+                result[index] = num;
+                index++;
+                freq[num]--;
+            }
+        }
+
+        for (int num = 0; num <= 1000; num++) {
+            while (freq[num] > 0) {
+                result[index] = num;
+                index++;
+                freq[num]--;
+            }
+        }
+
+        return result;
+    }
+}
